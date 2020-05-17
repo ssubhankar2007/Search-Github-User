c994c088e6ad2fdb42066d79f48b0907b4f493bc
Subhankar <ssubhankar2007@gmail.com>
2020-05-17 11:45:09 +0530

    Initial files

---
+++ Search_Github_User/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Search_Github_User
+//
+//  Created by Subhankar Ghosh on 17/05/20.
+//  Copyright © 2020 Subhankar Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Search_Github_UserTests/Search_Github_UserTests.swift
@@ -0,0 +1,34 @@
+//
+//  Search_Github_UserTests.swift
+//  Search_Github_UserTests
+//
+//  Created by Subhankar Ghosh on 17/05/20.
+//  Copyright © 2020 Subhankar Ghosh. All rights reserved.
+//
+
+import XCTest
+@testable import Search_Github_User
+
+class Search_Github_UserTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
